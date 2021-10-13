caf9b5b3cac62efae8b1f53164896afec7e6206a
Ariel <arieldelgadillo444@gmail.com>
2021-10-13 09:47:57 -0400

    estilos agregado a lis de convocatorias y código HTML de consultarConvocatorias.php modificado

--- backend/consultarConvocatorias.php
+++ backend/consultarConvocatorias.php
@@ -7,32 +7,54 @@ $result=mysqli_query($conexionBD,$query);
 $salida='';
 while ($row=mysqli_fetch_array($result)){
 $salida.='
+    
     <div class="card">
-      <div class="card-header">
-            <div class="card-title">
-                <h2> titulo: '.$row['TITULO_DOCUMENTO'].'</h2>
-            </div>
-
-            <div class="card-sa">
-                <h3> '.$row['SEMESTRE_ANIO'].'</h3>
-            </div>
-      </div>  
-            <div class="card-dates">
-                 <p>fecha de publicacion: '.$row['FECHA_INICIO'].'</p>
-                 <p>fecha limite: '.$row['FECHA_LIMITE'].'</p>
-            </div>
             
-            <div class="card-description">
-                    <p>'.$row['DESCRIPCION'].'</p>
+        <div class="card-sa">
+
+            <p> '.$row['SEMESTRE_ANIO'].'</p>
+
+            <div class="card-dates">
+                <p>Fecha de publicación: '.$row['FECHA_INICIO'].'</p>
+                <p>Fecha límite: '.$row['FECHA_LIMITE'].'</p>
             </div>
 
-         <a target="_blank" href="../archivos/'.$row['TITULO_DOCUMENTO'].'.pdf">revisar documento adjunto</a>
-         <hr>
-         <h3> pliego de especificaciones aun no publicado</h3>
-         <hr>
+        </div>
+
+        <div class="main-section-post-info">
+
+            <div class="main-section-post-info-photo">
+                <img src="../img/profile.png">
+            </div>
+
+            <div class="main-section-post-info-content">
+
+                <div class="main-section-post-info-content-convocatoria">
+                    <h3>Convocatoria</h3>
+                    <div class="card-title">
+                        <h4> Título: '.$row['TITULO_DOCUMENTO'].'</h4>
+                    </div>
+                    <p class="card-description">'.$row['DESCRIPCION'].'</p>
+                    <a target="_blank" href="../archivos/'.$row['TITULO_DOCUMENTO'].'.pdf">PDF convocatoria</a>
+                </div>
+
+                <div class="main-section-post-info-content-pliego">
+                    <h3>Pliego de especificaciones</h3>
+                    <div class="card-title">
+                        <h4></h4>
+                    </div>
+                    <p class="card-description"></p>
+                    <a href="">PDF pliego de especificaciones</a>
+                </div>
+
+            </div>
+        </div>
     </div>';
 }
 
-echo json_encode('<div class="cards">'.$salida.'</div>');
+echo json_encode('
+    <h1>Lista de convocatorias</h1>
+    <div class="cards">'.$salida.'</div>
+');
 
 ?>
